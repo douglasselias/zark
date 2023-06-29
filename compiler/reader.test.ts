edf559e62c04dee8b684a7b15685d3c78dd2fad3
Douglas S. Elias <douglassantoselias@gmail.com>
2023-06-28 23:58:56 -0300

    add reader test

--- compiler/reader.test.ts
+++ compiler/reader.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from '@jest/globals'
-import { tokenize } from './reader'
+import { tokenize, readTokens } from './reader'
 
 describe('tokenize', () => {
   it('no expression', () => {
@@ -57,5 +57,59 @@ describe('tokenize', () => {
   it('expression with string', () => {
     expect(tokenize('(append "hello " "world")')).toEqual
       (['(', 'append', '"hello "', '"world"', ')'])
+  })
+})
+
+describe('readTokens', () => {
+  it('no tokens', () => {
+    expect(readTokens([])).toEqual([])
+  })
+
+  it('single atom', () => {
+    expect(readTokens(['10'])).toEqual(10)
+  })
+
+  it('single atom', () => {
+    expect(readTokens(['pi'])).toEqual('pi')
+  })
+
+  it.skip('multiple atoms', () => {
+    expect(readTokens(['10', 'pi'])).toEqual([10, 'PI'])
+  })
+
+  it('single empty expression', () => {
+    expect(readTokens(['(', ')'])).toEqual([])
+  })
+
+  it('single parenthesis', () => {
+    expect(readTokens(['('])).toEqual([])
+  })
+
+  it('single right parenthesis', () => {
+    expect(() => readTokens([')'])).toThrowError()
+  })
+
+  it('single expression', () => {
+    expect(readTokens(['(', '1', '2', ')'])).toEqual([1, 2])
+  })
+
+  it('nested expression', () => {
+    expect(readTokens(['(', '+', '1', '2',
+      '(', '*', '3', '4', ')', ')'])).toEqual(['+', 1, 2, ['*', 3, 4]])
+  })
+
+  it('nested expression', () => {
+    expect(readTokens(['(', 'sum', '1', '2',
+      '(', 'prod', '3', '4', ')', ')'])).toEqual(['sum', 1, 2, ['prod', 3, 4]])
+  })
+
+  it('expression with variable/symbol', () => {
+    expect(readTokens(['(', 'list', '1', '2', ')'])).toEqual
+      (['list', 1, 2])
+  })
+
+  it('expression with string', () => {
+    expect(readTokens(['(', 'append', 'hello ', 'world', ')'])).toEqual
+      (['append', 'hello ', 'world'])
   })
 })
