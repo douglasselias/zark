6b45b10ce5d11e70f7aec8b34b0ccaee25685043
Douglas S. Elias <douglassantoselias@gmail.com>
2023-07-24 23:29:36 -0300

    add loop condition

--- c-zark/main.c
+++ c-zark/main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <editline/readline.h>
 #include <histedit.h>
 
 int main(int argc, char** argv) {
   puts("Zark Version 0.0.0");
-  puts("Press Ctrl+c to Exit or type (exit)\n");
+  puts("Press Ctrl+c to exit or type exit\n");
 
-  while (1) {
+  int shouldLoop = 1;
+  while (shouldLoop) {
     char* input = readline("zark > ");
     add_history(input);
     printf("Eval: %s\n", input);
+    if(strcmp(input, "exit") == 0)
+      shouldLoop = 0;
     free(input);
   }
 
